e05781d5b2284669a174405f7bc21228aec20d6d
Royde <roy512630@gmail.com>
2026-04-23 11:18:00 +0530

    Fix whitenoise, media config, STORAGES for Django 6.0 + Render deploy

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,10 +83,14 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static_cdn',
 ]
 
-# Whitenoise Storage 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# WHITENOISE_USE_FINDERS = True
-# WHITENOISE_MANIFEST_STRICT = False
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Cloudinary Settings
 CLOUDINARY_STORAGE = {
@@ -94,8 +98,6 @@ CLOUDINARY_STORAGE = {
     'API_KEY': '429592126147549',
     'API_SECRET': 'pP_PA6JfQ8ICDEul7CAkoPgDVJs',
 }
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
